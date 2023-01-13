4dcce6638ad6df020becb3c6ca956afbfc47360e
Valentine Borisevich <valentineborisevich@gmail.com>
2023-01-14 01:14:14 +0300

    task_029 finished

--- task_029/Program.cs
+++ task_029/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int[] FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+    return array;
+}
+
+int[] PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+
+int[] array_1 = new int[8];
+FillArray(array_1);
+PrintArray(array_1);
