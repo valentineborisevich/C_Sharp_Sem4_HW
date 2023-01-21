f1f871dae750719c447c68c39a7dd4b2525d195b
Valentine Borisevich <valentineborisevich@gmail.com>
2023-01-21 16:26:22 +0300

    HW finished

--- task_026/Program.cs
+++ task_026/Program.cs
@@ -3,3 +3,30 @@
 // 3, 5 -> 243 (3⁵)
 
 // 2, 4 -> 16
+
+// Console.WriteLine("Enter two numbers (A and B) and I will return A in B extent. Enter number A");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter number B");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int mult = 1;
+// for (int i = 0; i < b; i++)
+// {
+//     mult *= a;
+// }
+// Console.WriteLine(mult);
+int MLTPL(int number1, int number2)
+{
+    int mult = 1;
+    for (int i = 0; i < number2; i++)
+    {
+        mult *= number1;
+    }
+    return mult;
+}
+
+Console.WriteLine("Enter two numbers (A and B) and I will return A in B extent. Enter number A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number B");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = MLTPL(a, b);
+Console.WriteLine(c);
--- task_027/Program.cs
+++ task_027/Program.cs
@@ -6,3 +6,30 @@
 
 // 9012 -> 12
 
+
+
+// int SumOfDigits(int number)
+// {
+//     string array = Convert.ToString(number);
+//     int Length = array.Length;
+//     int sum = 0;
+//     for (int index = 1; index < Length; index++)
+//     {
+//         sum = sum + array[index];
+//     }
+// }
+// Console.WriteLine("Enter any number and I will return sum of it's digits.");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(SumOfDigits(a));
+
+
+Console.WriteLine("Enter any number and I will return sum of it's digits.");
+var a = Console.ReadLine();
+var array = Array.ConvertAll(a.ToString().ToCharArray(), x=> (int)Char.GetNumericValue(x));
+int Length = array.Length;
+int sum = 0;
+for (int i = 0; i < Length; i++)
+{
+    sum = sum + array[i];
+}
+Console.WriteLine(sum);
